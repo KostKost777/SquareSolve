bee5a2de570ecbd55c792b82fd012a4037d47ad8
Zinawe <121714420+KostKost777@users.noreply.github.com>
2025-08-28 15:03:55 +0300

    Delete unit_tests.h

--- unit_tests.h
+++
@@ -1,9 +0,0 @@
-#ifndef unit_tests
-#define unit_tests
-///@file
-//********************UnitTests**********************
-void test_solve_square();
-void test_solve_line();
-void test_double_is_same();
-
-#endif
